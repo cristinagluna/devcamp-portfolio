701e9bdded14d6f055f81b087bc9a5e0e279dc76
Jordan Hudgens <jordan@devcamp.com>
2017-07-31 16:39:31 +0000

    Updated portfolio to iclude attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
    access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
    
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
   
   def angular
@@ -67,6 +67,6 @@ class PortfoliosController < ApplicationController
  
  def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
-  end
+ end
 
 end
